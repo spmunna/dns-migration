5799693492af0d10d423b7d6818834773457c531
spmunna <spmunna@gmail.com>
2021-10-01 21:04:59 -0400

    initial version

---
+++ run-private.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+  
+python3 terrascript-private-zone-util.py
+
+for d in infoblox/*/ ; do
+    #echo "$d"
+    cd $d
+    terraform init
+    terraform apply
+    #terraform output -json | jq -r '@sh "export zoneid=\(.zone_ids.value)"'
+    zoneid=`terraform output -raw zone_id`
+    #echo $zoneid
+    domain=`terraform output -raw domain_name`
+    #echo $domain
+    cd ../..
+    aws route53 change-resource-record-sets --hosted-zone-id $zoneid  --change-batch file://mx/$domain.json
+done
+
+#cleanup
+#for d in infoblox/*/ ; do
+    #rm -rf $d
+#done   
+#for f in mx/*.json ; do
+    #rm -f $f
+#done
